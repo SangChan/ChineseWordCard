96cbdb365cbded46c5525817b39fcf9665c8293a
SangChan Lee <sangchan82@gmail.com>
2015-09-16 15:04:38 +0900

    add tap gesture

--- ChineseWordCard/ViewController.swift
+++ ChineseWordCard/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var pinyinLabel: UILabel!
+    @IBOutlet var hanyuLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var touchCount : Int = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+        resetAll();
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func resetAll() {
+        touchCount = 0;
+        setLabelHiddenByCount(touchCount);
+    }
+    
+    func setLabelHiddenByCount(count : Int) {
+        descriptionLabel.hidden = true;
+        pinyinLabel.hidden = true;
+        if(count == 0) {
+            return
+        }
+         
+        pinyinLabel.hidden = false;
+        if (count%2 == 0) {
+            descriptionLabel.hidden = false;
+        }
+        
+    }
+    
+    @IBAction func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            // handling code
+            setLabelHiddenByCount(++touchCount)
+        }
+    }
 }
 
