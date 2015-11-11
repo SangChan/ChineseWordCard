deb3e812e46044e336587679c463c9d6b80d2978
Sangchan Lee <sangchan82@gmail.com>
2015-11-11 10:34:55 +0800

    modify model

--- ChineseWordCard/Word+CoreDataProperties.swift
+++ ChineseWordCard/Word+CoreDataProperties.swift
@@ -2,8 +2,8 @@
 //  Word+CoreDataProperties.swift
 //  ChineseWordCard
 //
-//  Created by LeeSangchan on 2015. 11. 5..
-//  Copyright © 2015년 VerandaStudio. All rights reserved.
+//  Created by SangChan Lee on 11/11/15.
+//  Copyright © 2015 VerandaStudio. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -16,8 +16,9 @@ extension Word {
 
     @NSManaged var chapter: NSNumber?
     @NSManaged var desc: String?
+    @NSManaged var hanyu: String?
     @NSManaged var id: NSNumber?
     @NSManaged var level: NSNumber?
     @NSManaged var pinyin: String?
-    @NSManaged var hanyu: String?
+
 }
