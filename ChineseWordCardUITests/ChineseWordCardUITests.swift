fab259c11f65f881644df1805d2248e5bcd191a1
LeeSangchan <sangchan82@gmail.com>
2015-11-24 21:29:32 +0800

    add ui code

--- ChineseWordCardUITests/ChineseWordCardUITests.swift
+++ ChineseWordCardUITests/ChineseWordCardUITests.swift
@@ -31,6 +31,11 @@ class ChineseWordCardUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.buttons["Next"].tap()
+        app.buttons["Prev"].tap()
+        
     }
     
 }
