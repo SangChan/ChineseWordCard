11d3ce18d493e1dc717bc947407a05757fd57d38
Sangchan Lee <sangchan82@gmail.com>
2016-01-15 11:53:32 +0800

    Add controler for setting page.

---
+++ ChineseWordCard/SettingViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingViewController.swift
+//  ChineseWordCard
+//
+//  Created by SangChan Lee on 1/15/16.
+//  Copyright © 2016 VerandaStudio. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UITableViewController {
+    @IBAction func clickedDoneButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                // TODO : Show selection for 3 type of speak speed
+            }
+        }
+        else if indexPath.section == 1 {
+            
+        }
+    }
+}
