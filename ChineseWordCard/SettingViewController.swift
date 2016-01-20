175c83cbed8a38429cad2239911685e3e06f5019
Sangchan Lee <sangchan82@gmail.com>
2016-01-20 11:44:30 +0800

    Process at storyboard

--- ChineseWordCard/SettingViewController.swift
+++ ChineseWordCard/SettingViewController.swift
@@ -19,33 +19,11 @@ class SettingViewController: UITableViewController {
         if indexPath.section == 0 {
             if indexPath.row == 0 {
                 // TODO : Show selection for 3 type of speak speed
-                let pickerView = UIPickerView.init()
-                self.view.addSubview(pickerView)
             }
         }
         else if indexPath.section == 1 {
             
         }
     }
-    
-    func addPickerView() {
-        var pickerView = UIPickerView(frame: CGRectMake(0, 200, view.frame.width, 300))
-        pickerView.backgroundColor = .whiteColor()
-        pickerView.showsSelectionIndicator = true
-        
-        var toolBar = UIToolbar()
-        toolBar.barStyle = UIBarStyle.Default
-        toolBar.translucent = true
-        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
-        toolBar.sizeToFit()
-        
-        
-        var doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Bordered, target: self, action: "donePicker")
-        var spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
-        var cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Bordered, target: self, action: "canclePicker")
-        
-        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
-        toolBar.userInteractionEnabled = true
-        
-    }
+
 }
