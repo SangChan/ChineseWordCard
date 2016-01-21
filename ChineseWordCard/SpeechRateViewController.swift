b96fe03826c8fd320ccb866ece28a5782aeb2283
Sangchan Lee <sangchan82@gmail.com>
2016-01-21 18:18:24 +0800

    Add view controller for speech rate view

---
+++ ChineseWordCard/SpeechRateViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SpeechRateViewController.swift
+//  ChineseWordCard
+//
+//  Created by SangChan Lee on 1/21/16.
+//  Copyright © 2016 VerandaStudio. All rights reserved.
+//
+
+import UIKit
+
+class SpeechRateViewController: UITableViewController {
+
+}
