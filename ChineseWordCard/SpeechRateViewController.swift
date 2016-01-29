371cd7f016c3c58bb0e9abfe8d4fea3c1f349d28
Sangchan Lee <sangchan82@gmail.com>
2016-01-29 16:19:01 +0800

    code must be simple.

--- ChineseWordCard/SpeechRateViewController.swift
+++ ChineseWordCard/SpeechRateViewController.swift
@@ -10,44 +10,23 @@ import UIKit
 import AVFoundation
 
 class SpeechRateViewController: UITableViewController {
-    var previousSelect : NSIndexPath = NSIndexPath.init(forRow: 0, inSection: 0)
+    var previousSelect = NSIndexPath.init(forRow: 0, inSection: 0)
+    let rateArray = [AVSpeechUtteranceMinimumSpeechRate, AVSpeechUtteranceDefaultSpeechRate, AVSpeechUtteranceMaximumSpeechRate]
     // TODO : implement check mark and setting data to AppInfo object
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.cellForRowAtIndexPath(previousSelect)?.accessoryType = .None
-        if indexPath.section == 0 && indexPath.row == 0{
-            AppInfo.sharedInstance.speechRate = AVSpeechUtteranceMinimumSpeechRate
-        } else if indexPath.section == 0 && indexPath.row == 1 {
-            AppInfo.sharedInstance.speechRate = AVSpeechUtteranceDefaultSpeechRate
-        } else if indexPath.section == 0 && indexPath.row == 2 {
-            AppInfo.sharedInstance.speechRate = AVSpeechUtteranceMaximumSpeechRate
-        }
+        AppInfo.sharedInstance.speechRate = rateArray[indexPath.row]
         tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = .Checkmark
         tableView.deselectRowAtIndexPath(indexPath, animated: true);
         previousSelect = indexPath
     }
     
     override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-        if AppInfo.sharedInstance.speechRate ==  AVSpeechUtteranceMinimumSpeechRate {
-            if indexPath.section == 0 && indexPath.row == 0 {
-                cell.accessoryType = .Checkmark
-                previousSelect = indexPath
-            } else {
-                cell.accessoryType = .None
-            }
-        } else if AppInfo.sharedInstance.speechRate == AVSpeechUtteranceDefaultSpeechRate {
-            if indexPath.section == 0 && indexPath.row == 1 {
-                cell.accessoryType = .Checkmark
-                previousSelect = indexPath
-            } else {
-                cell.accessoryType = .None
-            }
-        } else if AppInfo.sharedInstance.speechRate == AVSpeechUtteranceMaximumSpeechRate {
-            if indexPath.section == 0 && indexPath.row == 2 {
-                cell.accessoryType = .Checkmark
-                previousSelect = indexPath
-            } else {
-                cell.accessoryType = .None
-            }
+        if AppInfo.sharedInstance.speechRate ==  rateArray[indexPath.row] {
+            cell.accessoryType = .Checkmark
+            previousSelect = indexPath
+        } else {
+            cell.accessoryType = .None
         }
     }
 }
