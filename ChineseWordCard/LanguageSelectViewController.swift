8fbeb0e5e147ce1909423af265f6feaf254130f1
LeeSangchan <sangchan82@gmail.com>
2016-02-21 16:01:19 +0800

    Add SelectLanguageViewController

---
+++ ChineseWordCard/LanguageSelectViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LanguageSelectViewController.swift
+//  ChineseWordCard
+//
+//  Created by LeeSangchan on 2016. 2. 21..
+//  Copyright © 2016년 VerandaStudio. All rights reserved.
+//
+
+import UIKit
+
+class LanguageSelectViewController: DetailSettingTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
