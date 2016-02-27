a8cc99c98ac53bbde624f9c1a1150da0e50ce703
LeeSangchan <sangchan82@gmail.com>
2016-02-27 23:13:29 +0800

    Show the check marks

--- ChineseWordCard/SortSelectViewController.swift
+++ ChineseWordCard/SortSelectViewController.swift
@@ -10,26 +10,7 @@ import UIKit
 
 class SortSelectViewController: DetailSettingTableViewController {
     let rateArray = ["Alphabet", "Star", "None"]
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        super.tableView(tableView, didSelectRowAtIndexPath: indexPath)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
