365b2df068f763aeeda27ec06aef803cff8fc18b
Sangchan Lee <sangchan82@gmail.com>
2016-02-29 17:11:20 +0800

    Test how to remove Pods folder

--- Pods/Target Support Files/Pods-ChineseWordCardTests/Pods-ChineseWordCardTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ChineseWordCardTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ChineseWordCardTestsVersionString[];
-
--- Pods/Target Support Files/Pods-ChineseWordCardUITests/Pods-ChineseWordCardUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChineseWordCardUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ChineseWordCardUITests
-@end
