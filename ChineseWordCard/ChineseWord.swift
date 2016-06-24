59a43d3cb15465f260ade037811086f36af1f0bf
Sangchan Lee <sangchan82@gmail.com>
2016-06-24 15:17:06 +0800

    Change default value for test something

--- ChineseWordCard/ChineseWord.swift
+++ ChineseWordCard/ChineseWord.swift
@@ -9,12 +9,12 @@
 import RealmSwift
 
 class ChineseWord: Object {
-    dynamic var id = 0
-    dynamic var level = 0
-    dynamic var chapter = 0
-    dynamic var desc_kr : String = ""
-    dynamic var desc_en : String = ""
-    dynamic var desc_es : String = ""
+    dynamic var id = -1
+    dynamic var level = -1
+    dynamic var chapter = -1
+    dynamic var desc_kr : String?
+    dynamic var desc_en : String?
+    dynamic var desc_es : String?
     dynamic var hanyu : String?
     dynamic var pinyin :String?
     dynamic var likeIt = false
