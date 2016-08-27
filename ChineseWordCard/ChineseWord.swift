e6ba51d40ffa78d51d88aa8059a21ae1d4f41241
SangChan Lee <sangchan82@gmail.com>
2016-08-27 20:46:29 +0800

    Unsigned Integer is not working on Swift

--- ChineseWordCard/ChineseWord.swift
+++ ChineseWordCard/ChineseWord.swift
@@ -9,9 +9,9 @@
 import RealmSwift
 
 class ChineseWord: Object {
-    dynamic var id : UInt = 0
-    dynamic var level : UInt = 0
-    dynamic var chapter : UInt = 0
+    dynamic var id : Int = 0
+    dynamic var level : Int = 0
+    dynamic var chapter : Int = 0
     dynamic var desc_kr : String?
     dynamic var desc_en : String?
     dynamic var desc_es : String?
