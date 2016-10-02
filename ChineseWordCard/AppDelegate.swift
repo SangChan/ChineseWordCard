5f0a1d09f1e3f78e27f4451bd702df4cc82d1350
SangChan Lee <sangchan82@gmail.com>
2016-10-02 19:26:04 +0800

    set property test for realm

--- ChineseWordCard/AppDelegate.swift
+++ ChineseWordCard/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Realm.Configuration.defaultConfiguration.deleteRealmIfMigrationNeeded = true
         AppInfo.sharedInstance.getAllDataFromRealm()
         return true
     }
