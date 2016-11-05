cfce54d115aa67a13c9b71fbc569ae81063e47ee
SangChan Lee <sangchan82@gmail.com>
2016-11-05 10:38:13 +0800

    add todo and log for never happen check

--- ChineseWordCard/LanguageSelectViewController.swift
+++ ChineseWordCard/LanguageSelectViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LanguageSelectViewController: DetailSettingTableViewController {
     override func setupData() {
+        // TODO : this gonna never happen on 1.0 release!
+        log("i hope never happens..")
         detailName = "LanguageSelect"
         details = [LanguageIndex.LanguageIndexEN,LanguageIndex.LangyageIndexES,LanguageIndex.LanguageIndexKR]
     }
