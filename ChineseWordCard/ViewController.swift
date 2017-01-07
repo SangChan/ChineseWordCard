519c9642b7f9c75e7b81ff562191161735c99d57
SangChan Lee <sangchan82@gmail.com>
2017-01-07 22:07:14 +0800

    remove comment

--- ChineseWordCard/ViewController.swift
+++ ChineseWordCard/ViewController.swift
@@ -40,7 +40,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         setButtonDefault()
         resetView()
         getWordData()
@@ -61,7 +60,6 @@ class ViewController: UIViewController {
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
