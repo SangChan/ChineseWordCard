8a5fe1af199c11eb7cfea02c65c6365d507cacc8
SangChan Lee <sangchan82@gmail.com>
2017-04-09 11:32:14 +0800

    change gap of decription

--- ChineseWordCard/ChineseWord.swift
+++ ChineseWordCard/ChineseWord.swift
@@ -9,15 +9,15 @@
 import RealmSwift
 
 class ChineseWord: Object {
-    dynamic var id      : Int = 0 //unique ID
-    dynamic var level   : Int = 0 //level of books
-    dynamic var chapter : Int = 0 //chapter from specific book
-    dynamic var desc_kr : String! //description from word for Korean
-    dynamic var desc_en : String? //description from word for English
-    dynamic var desc_es : String? //description from word for Espanol
-    dynamic var hanyu   : String! //hanyu from word
-    dynamic var pinyin  : String! //pinyin from word
+    dynamic var id      : Int = 0        //unique ID
+    dynamic var level   : Int = 0        //level of books
+    dynamic var chapter : Int = 0        //chapter from specific book
+    dynamic var desc_kr : String!        //description from word for Korean
+    dynamic var desc_en : String?        //description from word for English
+    dynamic var desc_es : String?        //description from word for Espanol
+    dynamic var hanyu   : String!        //hanyu from word
+    dynamic var pinyin  : String!        //pinyin from word
     dynamic var likeIt  : Bool = false   //value for star button
-    dynamic var play    : Int = 0 //how many play this word
-    dynamic var isShown : Bool = false  //shown to user
+    dynamic var play    : Int = 0        //how many play this word
+    dynamic var isShown : Bool = false   //shown to user
 }
