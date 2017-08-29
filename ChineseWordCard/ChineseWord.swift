b1e41e6b830f1b362e8a1528668c3ec2a4a89ba0
SangChan Lee <sangchan82@gmail.com>
2017-08-29 21:15:57 +0800

    default as non-nil

--- ChineseWordCard/ChineseWord.swift
+++ ChineseWordCard/ChineseWord.swift
@@ -13,8 +13,8 @@ class ChineseWord: Object {
     dynamic var level   : Int = 0        //level of books
     dynamic var chapter : Int = 0        //chapter from specific book
     dynamic var desc_kr : String!        //description from word for Korean
-    dynamic var desc_en : String?        //description from word for English
-    dynamic var desc_es : String?        //description from word for Espanol
+    dynamic var desc_en : String!        //description from word for English
+    dynamic var desc_es : String!        //description from word for Espanol
     dynamic var hanyu   : String!        //hanyu from word
     dynamic var pinyin  : String!        //pinyin from word
     dynamic var likeIt  : Bool = false   //value for star button
