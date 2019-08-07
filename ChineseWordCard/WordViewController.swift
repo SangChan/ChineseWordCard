12f2cd43a11c5e5c63e6f24a52f2120a7f03d2ff
Sangchan Lee <sangchan82@gmail.com>
2019-08-07 14:22:37 +0900

    extract some model to new file

--- ChineseWordCard/WordViewController.swift
+++ ChineseWordCard/WordViewController.swift
@@ -468,19 +468,3 @@ extension WordViewController {
             .disposed(by: disposeBag)
     }
 }
-
-struct WordViewModel {
-    var wordIndex = BehaviorSubject<Int>(value: 0)
-    var currentWord = BehaviorSubject<ChineseWord>(value: ChineseWord())
-    var touchCount = BehaviorSubject<Int>(value: 0)
-    var prevEnable = BehaviorSubject<Bool>(value: false)
-    var nextEnable = BehaviorSubject<Bool>(value: false)
-    var hanyu = BehaviorSubject<String>(value: "")
-    var desc = BehaviorSubject<String>(value: "")
-    var pinyin = BehaviorSubject<String>(value: "")
-    var likeIt = BehaviorSubject<Bool>(value: false)
-    var hanyuAlpha = BehaviorSubject<Float>(value: 1.0)
-    var descAlpha = BehaviorSubject<Float>(value: 0.0)
-    var pinyinAlpha = BehaviorSubject<Float>(value: 0.0)
-    var starButtonHidden = BehaviorSubject<Bool>(value: false)
-}
---
+++ ChineseWordCard/WordViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  WordViewModel.swift
+//  ChineseWordCard
+//
+//  Created by Sangchan Lee on 07/08/2019.
+//  Copyright © 2019 VerandaStudio. All rights reserved.
+//
+
+import RxSwift
+
+struct WordViewModel {
+    var wordIndex = BehaviorSubject<Int>(value: 0)
+    var currentWord = BehaviorSubject<ChineseWord>(value: ChineseWord())
+    var touchCount = BehaviorSubject<Int>(value: 0)
+    var prevEnable = BehaviorSubject<Bool>(value: false)
+    var nextEnable = BehaviorSubject<Bool>(value: false)
+    var hanyu = BehaviorSubject<String>(value: "")
+    var desc = BehaviorSubject<String>(value: "")
+    var pinyin = BehaviorSubject<String>(value: "")
+    var likeIt = BehaviorSubject<Bool>(value: false)
+    var hanyuAlpha = BehaviorSubject<Float>(value: 1.0)
+    var descAlpha = BehaviorSubject<Float>(value: 0.0)
+    var pinyinAlpha = BehaviorSubject<Float>(value: 0.0)
+    var starButtonHidden = BehaviorSubject<Bool>(value: false)
+}
