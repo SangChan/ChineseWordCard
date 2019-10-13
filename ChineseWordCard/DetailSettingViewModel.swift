3c931beb1f2950c6feab9632ea2ef2631dc2eb91
Sangchan Lee <sangchan82@gmail.com>
2019-10-13 10:15:49 +0900

    add new vm for project

---
+++ ChineseWordCard/DetailSettingViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DetailSettingViewModel.swift
+//  ChineseWordCard
+//
+//  Created by Sangchan Lee on 13/10/2019.
+//  Copyright © 2019 VerandaStudio. All rights reserved.
+//
+
+import Foundation
