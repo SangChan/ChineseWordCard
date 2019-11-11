d9563340199ce9fe9a632373b1c672386aeb6d30
Sangchan Lee <sangchan82@gmail.com>
2019-11-11 10:07:14 +0900

    remove success case delegate about google ad

--- ChineseWordCard/WordViewController.swift
+++ ChineseWordCard/WordViewController.swift
@@ -421,30 +421,9 @@ extension WordViewController {
 // MARK: - about Google Ads SDK
 
 extension WordViewController : GADBannerViewDelegate {
-    /// Tells the delegate an ad request loaded an ad.
-    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
-        //print("adViewDidReceiveAd")
-    }
-
     /// Tells the delegate an ad request failed.
     func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
         print("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
-    }
-
-    /// Tells the delegate that a full-screen view will be presented in response
-    /// to the user clicking on an ad.
-    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
-        //print("adViewWillPresentScreen")
-    }
-
-    /// Tells the delegate that the full-screen view will be dismissed.
-    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
-        //print("adViewWillDismissScreen")
-    }
-
-    /// Tells the delegate that the full-screen view has been dismissed.
-    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
-        //print("adViewDidDismissScreen")
     }
 
     /// Tells the delegate that a user click will open another app (such as
