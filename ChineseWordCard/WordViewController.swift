03eadf48e740949435a55135f692801251405215
Sangchan Lee <sangchan82@gmail.com>
2019-12-06 15:24:31 +0900

    remove useless cases

--- ChineseWordCard/WordViewController.swift
+++ ChineseWordCard/WordViewController.swift
@@ -219,7 +219,8 @@ extension WordViewController {
         guard let realm = self.lazyRealm else { return .none }
         switch sortIndex {
         case SortIndex.sortIndexStar :
-            return (realm.objects(ChineseWord.self).filter("likeIt == true").count > 0) ? realm.objects(ChineseWord.self).filter("likeIt == true") : realm.objects(ChineseWord.self)
+            //guard realm.objects(ChineseWord.self).filter("likeIt == true").count > 0 else { return  realm.objects(ChineseWord.self) }
+            return realm.objects(ChineseWord.self).filter("likeIt == true") 
         case SortIndex.sortIndexAlphabet :
             return realm.objects(ChineseWord.self).sorted(byKeyPath: "pinyin")
         default :
