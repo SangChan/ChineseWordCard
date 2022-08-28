c30b97c5e096b575ac9d317f5f88eb3f8fd87c01
Sangchan <sangchan82@gmail.com>
2022-08-28 15:08:45 +0900

    add new file about SwiftUI

---
+++ ChineseWordCard/SwiftUI/WordView.swift
@@ -0,0 +1,21 @@
+//
+//  WordView.swift
+//  ChineseWordCard
+//
+//  Created by sangchan on 2022/08/28.
+//  Copyright © 2022 VerandaStudio. All rights reserved.
+//
+
+import SwiftUI
+
+struct WordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WordView_Previews: PreviewProvider {
+    static var previews: some View {
+        WordView()
+    }
+}
